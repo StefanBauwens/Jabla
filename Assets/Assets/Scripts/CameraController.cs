e75a877d3324d2d033a601a808e14bee6f80a380
Stefan Bauwens <stefan.bauwens@hotmail.com>
2016-02-15 16:54:36 +0100

    File import

---
+++ Assets/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+    //camera won't directly follow player, but follows certain amount of time behind player --> gives a smoother image
+
+    public GameObject followTarget;                                       // make camera follow a specific object, not just the player
+    private Vector3 targetPos;                                            // position for object to aim towards 
+    public float moveSpeed;                                               // how fast camera will chase after player
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        targetPos = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z); // z gets default value because it can't have same value as the camera --> z from camera = -10 so the value of this z can't be -10 or else the game world would go on top of the camera
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);                        // Vector3.Lerp( current position of camera, where the player is, movement that camera can have in every update) 
+    }
+}
---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed; // set movement speed in Unity (public)
+
+    private Animator anim;
+
+    private bool playerMoving;
+    private Vector2 lastMove; // to set the direction of the face when not moving
+
+	// Use this for initialization
+	void Start () {                                            
+        anim = GetComponent<Animator>();         // it will search for Animator object
+	}
+
+    // Update is called once per frame                                                                                       //GetAxisRaw --> methode in Class Input | GetAxisRaw = a key that gets pressed
+    void Update ()                                                                                                           //right > 0.5f, left < -0.5f en 0 = idle (stilstaan) 
+    {
+        playerMoving = false;
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)                                 //moving horizontal (left & right) --> on the x-axis            
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));           //Translate --> lets the player move
+            playerMoving = true;                                                                                             //Vector3( x, y, z) | Time.deltaTime = time between every upate | * Time.deltaTime --> will take care of distance difference between different fps
+            lastMove = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)                                     //up > 0.5f en down < -0.5f
+        {                                                                                                                    //moving vertical (up & down) --> on the y-axis
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(0f, Input.GetAxisRaw("Vertical"));          
+        }
+
+        anim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));      // give a number to MoveX
+        anim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));        // give a number to MoveY
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);                      // x coordinate from variable lastMove
+        anim.SetFloat("LastMoveY", lastMove.y);                      // y coordinate from variable lastMove
+    }
+}
