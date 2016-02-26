71653c609b328513e87fc0fedeb012bcb77d01ef
Cindy Ho <Cindy Ho>
2016-02-26 18:54:40 +0100

    Changed Timer script so that we only have to input time in minutes.

--- Assets/Assets/Scripts/Timer.cs
+++ Assets/Assets/Scripts/Timer.cs
@@ -6,17 +6,26 @@ public class Timer : MonoBehaviour
 {
 
     public Text textTimer;
-    public float countdownTime;
-    
+    public float timeInMinutes;
+    float countdownTime;
 
+
+    void Start()
+    {
+        countdownTime = (timeInMinutes * 60);   //change minutes to seconds
+    }
     // Update is called once per frame
     void Update()
     {
-        countdownTime -= Time.deltaTime; //countdown
-        int minutes = Mathf.FloorToInt(countdownTime / 60f);//Math.FloorToInt --> number will be rounded down to the nearest integer(afronden)
+        countdownTime -= Time.deltaTime;
+
+        //Math.FloorToInt --> number will be rounded down to the nearest integer(afronden)
+        int minutes = Mathf.FloorToInt(countdownTime / 60f);    
         int seconds = Mathf.FloorToInt(countdownTime - minutes * 60);
+
+        //display mm:ss format on screen
         string displayTime = string.Format("{0:0}:{1:00}", minutes, seconds);
-        textTimer.text = displayTime;//let Text Object display in time format
+        textTimer.text = displayTime; 
         
     }
 }
