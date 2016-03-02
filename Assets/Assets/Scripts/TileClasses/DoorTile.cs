0b722dce9bbcebd2eb594315594ab22333aaa57b
Stefan Bauwens <stefan.bauwens@hotmail.com>
2016-03-02 09:36:47 +0100

    Added tile classes

---
+++ Assets/Assets/Scripts/TileClasses/DoorTile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorTile : Tiles { //e.G: doors that need a key
+
+	public string mInfo;//Information about what the "door" needs to be opened
+
+	//Constructors
+	public DoorTile() //Since this default constructor we're probably never going to use I guess I'll delete it later :P
+	{
+		mType = tileType.DoorTile;
+		mCanWalkThrough = false;
+		mInfo = "This door needs a key to be opened";
+	}
+
+	public DoorTile(string info)
+	{
+		mType = tileType.DoorTile;
+		mCanWalkThrough = false;
+		mInfo = info;
+	}
+
+	//Methods
+
+	public void CheckForKey()
+	{
+		//insert code to check if player has key. If so door changes that the player can walk through it.
+	}
+}
---
+++ Assets/Assets/Scripts/TileClasses/Teleport.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : Tiles {
+	
+	public int mNewXCoordinate;
+	public int mNewYCoordinate;
+
+	//Constructors
+	public Teleport()
+	{
+		mType = tileType.Teleport;
+		mNewXCoordinate = 0; //eventually change these to Player's current position
+		mNewYCoordinate = 0;
+	}
+
+	public Teleport(bool canWalkThrough, int newXCoordinate, int newYCoordinate) : base(canWalkThrough)
+	{
+		mType = tileType.Teleport;
+		mNewXCoordinate = newXCoordinate; 
+		mNewYCoordinate = newYCoordinate;
+	}
+
+	//properties
+	public int NewXCoordinate
+	{
+		get
+		{
+			return mNewXCoordinate;
+		}
+		set
+		{
+			mNewXCoordinate = value;
+		}
+	}
+
+	public int NewYCoordinate
+	{
+		get
+		{
+			return mNewYCoordinate;
+		}
+		set
+		{
+			mNewYCoordinate = value;
+		}
+	}
+}
