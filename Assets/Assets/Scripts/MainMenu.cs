50e92415b7bf162ed6273eec5f9562747607c163
Cindy Ho <cindy.ho8197@gmail.com>
2016-04-04 19:20:48 +0200

    Added Main Menu

---
+++ Assets/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+    public string startGame; // variable to put the game scene in
+
+
+    public void StartGame()
+    {
+        Application.LoadLevel(startGame);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+	
+}
