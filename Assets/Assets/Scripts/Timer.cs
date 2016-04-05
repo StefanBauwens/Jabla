63764390a87e3a6f0e63a3ccf3e8d8faffa591f5
Cindy Ho <cindy.ho8197@gmail.com>
2016-04-05 14:01:25 +0200

    Fixed Time Format Display :)))

--- Assets/Assets/Scripts/Timer.cs
+++ Assets/Assets/Scripts/Timer.cs
@@ -27,12 +27,12 @@ public class Timer : MonoBehaviour
         int seconds = Mathf.FloorToInt(countdownTime - minutes * 60);
         
         //display mm:ss format on screen
-        string displayTime = string.Format("{0}:{1}", minutes, seconds);
+        string displayTime = string.Format("{0:00}:{1:00}", minutes, seconds);
         textTimer.text = displayTime;
 
         //changeTime = timeInMinutes;
         
-        if (displayTime == "0:0")
+        if (minutes == 0 && seconds == 0)
         {
             timeScreen.timeUp = true; // time is up
             timeScreen.TimeUp(); // calls method TimeUp from class TimeMenu
