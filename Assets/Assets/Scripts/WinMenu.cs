8c07fdf4bb26c5f9ee9d1f6c40955919f77d610a
Cindy Ho <cindy.ho8197@gmail.com>
2016-04-08 16:38:03 +0200

    added win menu

---
+++ Assets/Assets/Scripts/WinMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WinMenu : MainMenu {
+
+    public GameObject winMenuCanvas; // win menu screen
+    public bool playerWon;
+
+
+
+    public void PlayerWinGame()
+    {
+        if (playerWon)
+        {
+            winMenuCanvas.SetActive(true); // show win menu
+        }
+        else
+        {
+            winMenuCanvas.SetActive(false);
+        }
+    }
+
+}
